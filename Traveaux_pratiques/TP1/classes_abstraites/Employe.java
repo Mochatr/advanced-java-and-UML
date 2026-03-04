f921e2a5f5d6b24d804ba5acf02b5c3f5b7e7e04
Mohamed Chatr <125512153+Mochatr@users.noreply.github.com>
2026-03-04 00:21:02 +0000

    Add abstract Employe class and Ingénieur subclass with salary calculation method

---
+++ Traveaux_pratiques/TP1/classes_abstraites/Employe.java
@@ -0,0 +1,25 @@
+abstract class Employe {
+    // Attributs
+    private String nom;
+    private String prenom;
+    private String email;
+    private String telephone;
+    private int salaire;
+
+    // constructeur
+    public Employe(String nom, String prenom, String email, String telephone, int salaire){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.telephone = telephone;
+        this.salaire = salaire;
+    }
+
+    // getters et setters
+    public int getSalaire() {
+        return this.salaire;
+    }
+
+    // Methode abstraite
+    public abstract void calculerSalaire();
+}
---
+++ Traveaux_pratiques/TP1/classes_abstraites/Ingénieur.java
@@ -0,0 +1,16 @@
+public class Ingénieur extends Employe {
+    // Attributs
+    private String spécialité;
+
+    // Contructeur
+    public Ingénieur(String nom, String prenom, String email, String telephone, int salaire, String spécialité) {
+        super(nom, prenom, email, telephone, salaire);
+        this.spécialité = spécialité;
+    }
+
+    // Implémentation de la méthode abstraite
+    @Override
+    public void calculerSalaire(){
+        int nvSalaire = getSalaire() + (int)(getSalaire() * 0.15);
+        System.out.println("Le salaire de l'ingénieur " + spécialité + " est : " + nvSalaire);
+}
